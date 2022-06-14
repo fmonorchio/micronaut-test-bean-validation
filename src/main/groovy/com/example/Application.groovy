d21f3a287e8613b2a4d442ace876a31e6897a4f3
fmonorchio <francesco@faire.ai>
2022-06-14 22:25:10 +0200

    Remove duplicate bean

--- src/main/groovy/com/example/Application.groovy
+++ src/main/groovy/com/example/Application.groovy
@@ -1,20 +1,12 @@
 package com.example
 
 import groovy.transform.CompileStatic
-import io.micronaut.context.MessageSource
-import io.micronaut.context.annotation.Bean
-import io.micronaut.context.i18n.ResourceBundleMessageSource
 import io.micronaut.runtime.Micronaut
 
 @CompileStatic
 class Application {
     static void main(String[] args) {
         Micronaut.run(Application, args)
-    }
-
-    @Bean
-    MessageSource messageSource() {
-        return new ResourceBundleMessageSource('messages')
     }
 
 }
